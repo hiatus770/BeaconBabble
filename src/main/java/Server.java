7876d9ea295a948b4756016f98d52d2c457397b1
Oliver <45010534+feelingboredexe@users.noreply.github.com>
2023-03-29 19:06:11 -0400

    server stuff added, needs some polishing

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,18 +2,22 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-
+/**
+ * Handles all the server duties.
+ * This includes initializing the server, broadcasting messages to all users, and keeping track of all the usernames.
+ * @author goose
+ */
 public class Server {
     private int port;
     private Set<String> usernames = new HashSet<>();
-    private Set<Thread> userThreads = new HashSet<>();
+    private Set<UserThread> userThreads = new HashSet<>();
 
     /**
+     * This method is called by the main method to initialize the server
      * @param port the port number for the server to listen on
      *             (must be between 0 and 65535)
      * @throws IOException if an I/O error occurs when opening the socket
      * @throws IllegalArgumentException if the port parameter is outside the specified range of valid port values (but this likely won't happen)
-     * This method is called by the main method to initialize the server
      */
     public void init(int port) {
         try {
@@ -23,6 +27,62 @@ public class Server {
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port " + port + " or listening for a connection");
         }
+    }
+
+    /**
+     * This method is called by the UserThread class to send a message to all users.
+     * The method loops through all the users in the ArrayList of user threads and sends the message to each user.
+     * @param message the message to be sent
+     * @param thread the thread that sent the message
+     */
+    public void broadcast(String message, UserThread thread) {
+        for (UserThread user : userThreads) {
+            if (user != thread) {
+                user.sendMessage(message);
+            }
+        }
+    }
+
+    /**
+     * This method is called by the UserThread class to add a user
+     * and their respective user thread from the ArrayList of usernames and user threads.
+     * @param username the username of the user to be removed
+     */
+    public void addUsername(String username, UserThread userThread) {
+        usernames.add(username);
+        userThreads.add(userThread);
+    }
+
+    /**
+     * This method is called by the UserThread class to remove a user
+     * and their respective user thread from the ArrayList of usernames and user threads.
+     * @param username the username of the user to be removed
+     */
+    public void removeUsername(String username, UserThread userThread) {
+        usernames.remove(username);
+        userThreads.remove(userThread);
+    }
+
+    /**
+     * This method is called by the UserThread class to check if there are any users connected to the server.
+     * @return true if there are users connected to the server, false otherwise
+     */
+    public boolean hasUsers() {
+        return !this.usernames.isEmpty();
+    }
+
+    /**
+     * This method is called by the UserThread class to get a list of all the usernames connected to the server.
+     * @return a formatted string containing all the usernames connected to the server
+     */
+    public String getUsernames() {
+        // matias this is kind of a shitty way to do this
+        // it works for now, but if you want to you can look into other ways to do this
+        String usernameList = "";
+        for (String username : usernames) {
+            usernameList += username + "\n";
+        }
+        return usernameList;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/UserThread.java
@@ -0,0 +1,77 @@
+import java.net.*;
+import java.io.*;
+
+/**
+ * This thread is responsible for handling all the communication with a single client.
+ * This way, the server can handle multiple clients at the same time.
+ * In simple terms, this class represents each user.
+ * @author goose
+ */
+public class UserThread extends Thread {
+    private Socket socket;
+    private Server server;
+    private PrintWriter writer;
+
+    public UserThread(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    /**
+     * Runs the user thread. Handles all the interactions the user thread should be able to do with the server.
+     */
+    public void run() {
+        try {
+            InputStream input = socket.getInputStream(); // returns an input stream for this socket
+
+            // Reads text from the character-input stream above
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            OutputStream output = socket.getOutputStream(); // returns the output stream for the socket
+            writer = new PrintWriter(output, true);
+
+            String username = reader.readLine(); // obtains username from the client
+            server.addUsername(username, this); // adds the username to the set of usernames
+
+            String serverMessage = "New user connected: " + username + ". Welcome!";
+            server.broadcast(serverMessage, this); // Broadcasts the newly connected user to all users
+
+            String clientMessage;
+
+            do {
+                clientMessage = reader.readLine(); // receives the client message
+                serverMessage = "[" + username + "]: " + clientMessage; // formatting client message
+                server.broadcast(serverMessage, this); // broadcasts the client message to all users
+            } while (!clientMessage.equals("/exit"));
+
+            server.removeUsername(username, this); // removes the username from the set of usernames after they DC
+            socket.close(); // close le socket
+
+            serverMessage = username + " has left the room."; // bye bye message
+            server.broadcast(serverMessage, this);
+
+        } catch (IOException e) {
+            System.out.println("Error in UserThread: " + e.getMessage()); // uh oh poopy stinky
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Prints a list of online users to the newly connected user.
+     */
+    public void printUsers() {
+        if (server.hasUsers()) {
+            writer.println("Connected users: " + server.getUsernames());
+        } else {
+            writer.println("No other users connected");
+        }
+    }
+
+    /**
+     * Sends a message from the user to the server for broadcast.
+     * @param message The message to be sent, in a String format.
+     */
+    public void sendMessage(String message) {
+        writer.println(message);
+    }
+}
