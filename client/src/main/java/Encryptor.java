eee47767ffb6584d0b0153f2ac4fc542dede5d85
Jerrywu4023 <jerrywu4023@gmail.com>
2023-04-11 10:50:26 -0400

    added comments(?)

--- client/src/main/java/Encryptor.java
+++ client/src/main/java/Encryptor.java
@@ -1,1 +1,13 @@
 
+/**
+ * Encryptor class
+ * Encrypts and decrypts messages
+ * OR or XOR encryption algorithim
+ * key updated according to "user number" (or "time")
+ * @param key key for encryption
+ * @author jerrywu
+ */
+
+public class Encryptor {
+
+}
